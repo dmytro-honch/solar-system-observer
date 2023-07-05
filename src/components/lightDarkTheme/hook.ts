16d7920b5e1de8d3c85d5eb3c6cc3df00a5d7e07
Oleksii Moroz <47codemonkey@gmail.com>
2023-07-05 22:26:28 +0300

    final fixes

--- src/components/lightDarkTheme/hook.ts
+++ src/components/lightDarkTheme/hook.ts
@@ -1,34 +1,3 @@
-// import { useState, useEffect, useCallback } from 'react';
-// import { THEMES } from 'src/constants';
-
-
-// export const handleToggleTheme = () => {
-
-//     const defaultTheme = window.matchMedia('(prefers-color-scheme: dark)')?.matches ? THEMES.dark : THEMES.light;
-//     const [theme, setTheme] = useState(localStorage.getItem('theme') || defaultTheme);
-    
-//     const toggleTheme = useCallback(() => {
-//       const newTheme = theme === THEMES.dark ? THEMES.dark : THEMES.light;
-//       setTheme(newTheme);
-//       localStorage.setItem('theme', newTheme);
-//     }, [theme]);
-    
-//     useEffect(() => {
-//       applyThemeStyles();
-//     }, [theme]);
-    
-//     const applyThemeStyles = () => {
-//       const body = document.body;
-//       if (theme === THEMES.light) {
-//         body.setAttribute('data-theme', THEMES.light);
-//         return
-//       } 
-//       body.setAttribute('data-theme', THEMES.dark);
-//     };
-    
-//     return { toggleTheme, applyThemeStyles };
-// };
-
 import { useState, useEffect, useCallback } from 'react';
 import { THEMES } from 'src/constants';
 
@@ -36,21 +5,21 @@ export const useToggleTheme = () => {
   const defaultTheme = window.matchMedia('(prefers-color-scheme: dark)')?.matches ? THEMES.dark : THEMES.light;
   const [theme, setTheme] = useState(localStorage.getItem('theme') || defaultTheme);
 
+  const applyThemeStyles = (currentTheme) => {
+    const body = document.body;
+    body.setAttribute('data-theme', currentTheme);
+  };
+
+  useEffect(() => {
+    applyThemeStyles(theme);
+  }, [theme]);
+
   const handleToggleTheme = useCallback(() => {
     const newTheme = theme === THEMES.dark ? THEMES.light : THEMES.dark;
     setTheme(newTheme);
     localStorage.setItem('theme', newTheme);
     applyThemeStyles(newTheme);
   }, [theme]);
-
-  useEffect(() => {
-    applyThemeStyles(theme);
-  }, [theme]);
-
-  const applyThemeStyles = (currentTheme) => {
-    const body = document.body;
-    body.setAttribute('data-theme', currentTheme);
-  };
 
   return handleToggleTheme;
 };
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,2 +1,2 @@
 export * from './planetObserver';
-export * from './lightDarkTheme';
+export * from './themes';
