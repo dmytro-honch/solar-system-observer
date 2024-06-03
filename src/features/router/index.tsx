e016a168c32d208a754a702f9f7d6fd7ad4c19cd
Oleksii <47codemonkey@gmail.com>
2024-06-03 22:28:50 +0300

    deleted some unnecessary files

--- src/features/router/index.tsx
+++ src/features/router/index.tsx
@@ -5,16 +5,8 @@ import MenuPage from 'src/features/menuPage';
 import Page404 from 'src/features/page404';
 import ContactUsPage from 'src/features/contactUsPage';
 import AboutProjectPage from 'src/features/aboutProjectPage';
-import SolarSystemObjectsPage from 'src/features/solarSystemObjectsPage';
 
-import {
-  MENU_PATH,
-  PLANET_OBSERVER_PATH,
-  FALLBACK_404_ROUTE,
-  CONTACT_US_PATH,
-  ABOUT_US_PATH,
-  SOLAR_SYSTEM_PATH,
-} from './path';
+import { MENU_PATH, PLANET_OBSERVER_PATH, FALLBACK_404_ROUTE, CONTACT_US_PATH, ABOUT_US_PATH } from './path';
 
 const Router = () => (
   <BrowserRouter>
@@ -24,7 +16,6 @@ const Router = () => (
       <Route path={ABOUT_US_PATH} element={<AboutProjectPage />} />
       <Route path={FALLBACK_404_ROUTE} element={<Page404 />} />
       <Route path={CONTACT_US_PATH} element={<ContactUsPage />} />
-      <Route path={SOLAR_SYSTEM_PATH} element={<SolarSystemObjectsPage />} />
     </Routes>
   </BrowserRouter>
 );
