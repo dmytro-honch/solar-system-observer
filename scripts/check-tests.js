c1ae9b394d0017a43c188990393dc0a8d3178bd3
Dmytro Honcharov <dmytro.honch@gmail.com>
2024-06-07 19:20:12 -0500

    add pre-commit hook

---
+++ scripts/check-tests.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+
+const componentsDir = path.join(__dirname, 'src/components');
+
+fs.readdir(componentsDir, (err, files) => {
+  if (err) {
+    console.error('Could not list the directory.', err);
+    process.exit(1);
+  }
+
+  files.forEach((file, index) => {
+    const componentFile = path.join(componentsDir, file);
+    const testFile = path.join(componentsDir, '__tests__', `${file}.test.js`);
+
+    fs.access(testFile, fs.constants.F_OK, (err) => {
+      if (err) {
+        console.error(`Test file not found for component ${file}`);
+        process.exit(1);
+      }
+    });
+  });
+});
